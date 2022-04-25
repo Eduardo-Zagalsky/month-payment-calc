56f8156898c0c7b0c1fb0ae8f73d6e31c94863d5
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2022-04-25 16:15:20 -0500

    added the unit test to validate math and decimal places

--- calculator-test.js
+++ calculator-test.js
@@ -1,11 +1,15 @@
 
 it('should calculate the monthly rate correctly', function () {
   // ...
+  const values = { amount: 1000, rate: 11, term: 3 };
+  expect(calculateMonthlyPayment(values)).toEqual("32.74");
 });
 
 
-it("should return a result with 2 decimal places", function() {
+it("should return a result with 2 decimal places", function () {
   // ..
+  const values = { amount: 9000, rate: 12, term: 5 };
+  expect(calculateMonthlyPayment(values)).toEqual("200.20");
 });
 
 /// etc
